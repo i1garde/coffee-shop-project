ed6b2988dc3c34e582bf6069a7eba23d0d1f8000
Ivan <vanyakomarenko@gmail.com>
2021-06-21 15:34:07 +0300

    Initial commit

---
+++ CoffeeShop.DAL/CoffeeShopDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace CoffeeShop.DAL
+{
+    public class CoffeeShopDBContext : DbContext
+    {
+        public CoffeeShopDBContext() : base("CoffeeShopDB") { }
+        
+        public DbSet<CoffeeMachineEntity> CoffeeMachine { get; set; }
+        public DbSet<IngredientEntity> Ingredient { get; set; }
+        public DbSet<IngredientsInRecipeEntity> IngredientsInRecipe { get; set; }
+        public DbSet<IngredientsInCoffeeMachineEntity> IngredientsInCoffeeMachine { get; set; }
+        public DbSet<RecipeEntity> Recipe { get; set; }
+        public DbSet<OrderEntity> Order { get; set; }
+    }
+}
